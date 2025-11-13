0016e87f306439e3af5b40220e2ca3359e3cccf9
yash1 <yash056440@gmail.com>
2025-11-13 10:26:38 +0530

    1

--- p1.py
+++ p1.py
@@ -1,4 +1,14 @@
-for row in range(5,1):
-    for col in range(1,6-row):
-        print("*", end=" ")
-    print()
+num = int(input("Enter number:"))
+
+for row in range(1,num+1):
+   
+    for col in range(num-row):
+        print(" ", end="")
+    for col in range(0,row):
+        print("*", end="")
+    
+    print(" ")
+
+
+
+
